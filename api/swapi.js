748d605d56c96534b9cb35901520f23ead5f96c7
Radik <radislaw22@gmail.com>
2019-09-17 14:56:32 +1000

    refactor: cleanup

--- api/swapi.js
+++
@@ -1,8 +0,0 @@
-// const ROOT_URL = 'https://swapi.co/api'
-//
-// export const getStarships = async ($axios) => {
-//   /* eslint-disable */
-//   console.log('$axios', $axios)
-//   const starships = await $axios.$get(`${ROOT_URL}/starships/`)
-//   return { starships }
-// }
